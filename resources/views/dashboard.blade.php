93cccab397f36d701b8d115380ff855cbc0e62b2
rahul p <rahulyts5@gmail.com>
2026-05-12 10:32:54 +0530

    fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,22 @@
 @section('title', 'Dashboard')
 
 @section('content')
-<div class="card shadow-sm">
+<div class="mt-3 d-flex justify-content-end gap-2">
+    @if(auth()->user()->hasRole('superadmin', 'admin'))
+        <a href="/invitations/create" class="btn btn-success">Invite User</a>
+    @endif
+
+    @if(auth()->user()->hasRole('admin', 'member'))
+        <a href="/urls/create" class="btn btn-primary">Create Short URL</a>
+    @endif
+
+    @if(auth()->user()->hasRole('admin','superadmin'))
+        <a href="/urls" class="btn btn-outline-primary">View URLs</a>
+    @else
+        <a href="/urls" class="btn btn-outline-primary">My URLs</a>
+    @endif
+</div>
+<div class="card shadow-sm mt-3">
     <div class="card-body">
         <h5>Welcome, {{ auth()->user()->name }}</h5>
         <p class="text-muted">
@@ -14,19 +29,5 @@
     </div>
 </div>
 
-<div class="mt-3">
-    @if(auth()->user()->hasRole('superadmin', 'admin'))
-        <a href="/invitations/create" class="btn btn-success">Invite User</a>
-    @endif
 
-    @if(auth()->user()->hasRole('admin', 'member'))>
-    <a href="/urls/create" class="btn btn-primary">Create Short URL</a>
-    @endif
-
-    @if(auth()->user()->hasRole('admin','superadmin'))
-        <a href="/urls" class="btn btn-outline-primary">View URLs</a>
-    @else
-        <a href="/urls" class="btn btn-outline-primary">My URLs</a
-    @endif
-</div>
 @endsection
